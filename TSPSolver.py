5002a366b0f751ea69f6e5576c45a970bd1b6731
Cameron Brown <cameronjoebrown@gmail.com>
2021-04-13 21:12:48 -0600

    Finished my version of Nearest Insertion Algorithm

--- TSPSolver.py
+++ TSPSolver.py
@@ -122,7 +122,6 @@ class TSPSolver:
 		route = []
 		random_index = np.random.randint(0, len(cities))
 		first_city = cities_not_in_route[random_index]
-		print("First city: " + first_city._name)
 		route.append(first_city)
 		cities_not_in_route.remove(first_city)
 		while not foundTour and time.time() - start_time < time_allowance:
@@ -133,29 +132,26 @@ class TSPSolver:
 			nearest_distance = np.inf
 			for i in range(len(route)):
 				if i == len(route) - 1:
-					i2 = 0
+					next_city = 0
 				else:
-					i2 = i + 1
-				currEdge = route[i].costTo(route[i2])
+					next_city = i + 1
+				distance = route[i].costTo(route[next_city])
 				for city_not_in_route in cities_not_in_route:
-					current_distance = (route[i].costTo(city_not_in_route) + city_not_in_route.costTo(route[i2])) - currEdge
-					if current_distance < nearest_distance:
+					cost_to_insert = (route[i].costTo(city_not_in_route) + city_not_in_route.costTo(route[next_city])) - distance
+					if cost_to_insert < nearest_distance:
 						nearest_city = city_not_in_route
 						nearest_city_index = cities_not_in_route.index(nearest_city)
-						nearest_distance = current_distance
+						nearest_distance = cost_to_insert
 						insert_index = i + 1
 
 			route.insert(insert_index, nearest_city)
 			del cities_not_in_route[nearest_city_index]
-			for i in route:
-				print(i._name)
-
-			if len(route) == len(cities):
-				# Found a valid route
-				foundTour = True
 
 			bssf = TSPSolution(route)
 			count += 1
+			if len(route) == len(cities):
+				# Found a valid route
+				foundTour = True
 		end_time = time.time()
 		results['cost'] = bssf.cost if foundTour else math.inf
 		results['time'] = end_time - start_time
