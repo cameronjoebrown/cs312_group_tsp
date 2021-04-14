5799f1564432fc119dd7ad2c21046ac37f44d074
Cameron Brown <cameronjoebrown@gmail.com>
2021-04-13 20:48:43 -0600

    Finished my version of Nearest Insertion Algorithm

--- TSPSolver.py
+++ TSPSolver.py
@@ -37,7 +37,7 @@ class TSPSolver:
 		solution found, and three null values for fields not used for this 
 		algorithm</returns> 
 	'''
-	
+
 	def defaultRandomTour( self, time_allowance=60.0 ):
 		results = {}
 		cities = self._scenario.getCities()
@@ -83,9 +83,9 @@ class TSPSolver:
 
 	def greedy( self,time_allowance=60.0 ):
 		pass
-	
-	
-	
+
+
+
 	''' <summary>
 		This is the entry point for the branch-and-bound algorithm that you will implement
 		</summary>
@@ -94,7 +94,7 @@ class TSPSolver:
 		not include the initial BSSF), the best solution found, and three more ints: 
 		max queue size, total number of states created, and number of pruned states.</returns> 
 	'''
-		
+
 	def branchAndBound( self, time_allowance=60.0 ):
 		pass
 
@@ -108,10 +108,64 @@ class TSPSolver:
 		best solution found.  You may use the other three field however you like.
 		algorithm</returns> 
 	'''
-		
+
 	def fancy( self,time_allowance=60.0 ):
-		pass
-		
+		results = {}
+		cities = self._scenario.getCities()
+		foundTour = False
+		count = 0
+		bssf = None
+		start_time = time.time()
+
+		# Add first city randomly to route
+		cities_not_in_route = cities.copy()
+		route = []
+		random_index = np.random.randint(0, len(cities))
+		first_city = cities_not_in_route[random_index]
+		print("First city: " + first_city._name)
+		route.append(first_city)
+		cities_not_in_route.remove(first_city)
+		while not foundTour and time.time() - start_time < time_allowance:
+			# Now build the route
+			nearest_city = None
+			nearest_city_index = None
+			insert_index = None
+			nearest_distance = np.inf
+			for i in range(len(route)):
+				if i == len(route) - 1:
+					i2 = 0
+				else:
+					i2 = i + 1
+				currEdge = route[i].costTo(route[i2])
+				for city_not_in_route in cities_not_in_route:
+					current_distance = (route[i].costTo(city_not_in_route) + city_not_in_route.costTo(route[i2])) - currEdge
+					if current_distance < nearest_distance:
+						nearest_city = city_not_in_route
+						nearest_city_index = cities_not_in_route.index(nearest_city)
+						nearest_distance = current_distance
+						insert_index = i + 1
+
+			route.insert(insert_index, nearest_city)
+			del cities_not_in_route[nearest_city_index]
+			for i in route:
+				print(i._name)
+
+			if len(route) == len(cities):
+				# Found a valid route
+				foundTour = True
+
+			bssf = TSPSolution(route)
+			count += 1
+		end_time = time.time()
+		results['cost'] = bssf.cost if foundTour else math.inf
+		results['time'] = end_time - start_time
+		results['count'] = count
+		results['soln'] = bssf
+		results['max'] = None
+		results['total'] = None
+		results['pruned'] = None
+		return results
+
 
 
 
